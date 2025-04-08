a99bf929a38366393c2153782931d969d49a6301
marianolongoort <mariano.longo@ort.edu.ar>
2025-04-08 19:48:24 -0300

    Creación proyecto base

---
+++ Estacionamiento.C/EstacionamientoMVC.C/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EstacionamientoMVC.C.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
